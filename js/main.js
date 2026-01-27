7c51710414c4dc787d56e5c07066e311d4670621
machida-stadium <machidaz@outlook.jp>
2026-01-27 18:04:54 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const NEWS_DATA = [
         category: "Notice",
         title: "「町田駅周辺にスタジアムを推進する会」公式サイトを仮オープンいたしました。",
         description: "本日、公式サイトを仮公開いたしました。町田駅周辺へのスタジアム構想の情報を順次発信してまいります。",
-        image: "images/news-001.png", // 画像がない場合は自動で「NO IMAGE」になります
+        image: "../images/news-001.png", // 画像がない場合は自動で「NO IMAGE」になります
         url: "news-001.html"
     }
 ];
@@ -187,3 +187,4 @@ function copyPageLink() {
     }
 
 }
+
